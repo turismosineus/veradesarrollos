da59de2f10b633fbb511f0e51d9d90479f254c34
turismosineus <turismosineus@gmail.com>
2026-09-11 12:07:42 -0300

    Delete state.js

--- state.js
+++
@@ -1,9 +0,0 @@
-// Estado de la aplicación.
-export const D = { projects:[], providers:[], expenses:[], liquidaciones:[], investments:[], investorDocs:[] };
-export const S = {
-  page:'login', user:null, modal:null, tab:'estado',
-  proj:null, prov:null, order:null, inv:null, invName:'',
-  revCat:'', revName:'', editId:null, provFilter:'todos',
-  mi:[{desc:'',qty:'',unit:'u',price:''}]
-};
-export const ui = { pendingFile:null, pendingFiles:[] };
